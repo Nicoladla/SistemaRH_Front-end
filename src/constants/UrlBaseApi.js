c426762da92ce2a77174d33c8b6b2a3ab2cca994
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-07-12 22:40:56 -0300

    feat: user registration in the database

---
+++ src/constants/UrlBaseApi.js
@@ -0,0 +1,3 @@
+const URL_BASE_API = "http://localhost:8080/api/v1/hiring";
+
+export default URL_BASE_API;
--- src/pages/RegisterCandidate.js
+++ src/pages/RegisterCandidate.js
@@ -1,21 +1,57 @@
+import { useState } from "react";
 import { styled } from "styled-components";
+import axios from "axios";
+import URL_BASE_API from "../constants/UrlBaseApi.js";
 
 export default function RegisterCandidate() {
+  const [candidate, setCandidate] = useState({ nome: "" });
+  const [isRegisteringCandidate, setIsRegisteringCandidate] = useState(false);
+
+  function updateNameCandidate(e) {
+    setCandidate({ ...candidate, [e.target.name]: e.target.value });
+  }
+
+  async function postCandidate(e) {
+    e.preventDefault();
+    setIsRegisteringCandidate(true);
+
+    try {
+      await axios.post(`${URL_BASE_API}/start`, candidate);
+      setIsRegisteringCandidate(false);
+      setCandidate({ ...candidate, nome: "" });
+      alert("Candidato cadastrado com sucesso.");
+    } catch (err) {
+      setIsRegisteringCandidate(false);
+      alert(err.response.data.message);
+    }
+  }
+
   return (
     <main>
       <Title>Cadastre candidatos para o seu processo seletivo</Title>
-      <Form>
+      <Form onSubmit={postCandidate}>
         <label>Nome do candidato:</label>
-        <input type="text" minLength={3} placeholder="Ex: Fulano" />
-        <button>Cadastrar</button>
+        <input
+          type="text"
+          placeholder="Ex: Fulano"
+          name="nome"
+          value={candidate.nome}
+          onChange={updateNameCandidate}
+          disabled={isRegisteringCandidate}
+          minLength={3}
+          required
+        />
+        <button type="submit" disabled={isRegisteringCandidate}>
+          Cadastrar
+        </button>
       </Form>
     </main>
   );
 }
 
-const Title= styled.h1`
-    margin-bottom: 30px;
-`
+const Title = styled.h1`
+  margin-bottom: 30px;
+`;
 
 const Form = styled.form`
   background-color: antiquewhite;
@@ -23,8 +59,7 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
 
-  input{
+  input {
     margin: 5px 0 10px;
   }
 `;
-
